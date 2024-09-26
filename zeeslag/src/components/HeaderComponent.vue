38636bf40739a7ae79e3cdac238dc8d518c810db
Danann Bartels <danann.bartels@hva.nl>
2024-09-26 13:59:17 +0200

    Fixed layout

---
+++ zeeslag/src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+    <div class="relative before:absolute before:w-full before:h-full before:inset-0 before:bg-black before:opacity-50 before:z-10">
+      <img src="@/assets/logo-zeeslag.jpg" alt="Banner Image" class="absolute inset-0 w-full h-full object-cover" />
+      <div class="min-h-[350px] relative z-50 h-full max-w-6xl mx-auto flex flex-col justify-center items-center text-center text-white p-6">
+        <h2 class="sm:text-4xl text-2xl font-bold mb-6">Zeeslag</h2>
+      </div>
+    </div>
+    <NavbarComponent/>
+  </header>
+</template>
+
+<script>
+import NavbarComponent from './NavbarComponent.vue';
+export default {
+  components: { NavbarComponent },
+    name: 'Header'
+}
+</script>
+
+
---
+++ zeeslag/src/components/NavbarComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="bg-gray-800 p-4">
+      <ul class="flex space-x-4">
+        <li><RouterLink to="/" class="text-white hover:text-gray-400">Home</RouterLink></li>
+        <li><RouterLink to="/about" class="text-white hover:text-gray-400">About</RouterLink></li>
+        <li><RouterLink to="/login" class="text-white hover:text-gray-400">Log In</RouterLink></li>
+        <li><RouterLink to="/register" class="text-white hover:text-gray-400">Register</RouterLink></li>
+        <li><RouterLink to="/play" class="text-white hover:text-gray-400">Play Now</RouterLink></li>
+      </ul>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar'
+  }
+  </script>
+  
+  <style scoped>
+  </style>
