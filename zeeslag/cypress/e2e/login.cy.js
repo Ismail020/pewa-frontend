7795c5152705ebbbb90d8988f8de1ca80c983851
Elena Lazareva <elena.lazareva@hva.nl>
2024-09-30 15:05:58 +0200

    Second cypress test reworked and extended

--- zeeslag/cypress/e2e/login.cy.js
+++ zeeslag/cypress/e2e/login.cy.js
@@ -3,11 +3,16 @@ describe('Login Function Test', () => {
         cy.visit('localhost:5173/login')
         cy.contains('button', 'LOGIN').should('exist')
     })
-    it('checks if the login redirects to /play on ok response from server', () => {
+    it('checks if the login redirects to /play on ok response from server and stays on same page otherwise', () => {
         cy.visit('localhost:5173/login')
         cy.get('input[type=text]').type('jd@gmail.com')
         cy.get('input[type=password]').type('password')
         cy.get('button[type="submit"]').click()
-        cy.url().should('include', "play")
+        cy.intercept('http://localhost:5173/login', () => {
+            if (!response.ok) {
+                cy.url().contains('play')
+            } else cy.url().contains('login')
+            cy.get('#hidden').invoke('show')
+        })
     })
 })
