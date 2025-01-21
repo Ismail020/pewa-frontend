04580df738dcd467bd6a409c5283ec4561d5a2ec
Shahin Shahandeh <shahin.shahandeh@hva.nl>
2025-01-21 20:40:12 +0100

    Pre moving

--- zeeslag/src/components/GameUI/BattleShipGame.vue
+++ zeeslag/src/components/GameUI/BattleShipGame.vue
@@ -15,6 +15,9 @@
       <LogComponent :title="'P2 Log'" :moves="p2Moves" class="max-w-xs" />
     </main>
     <ChatComponent :messages="chatMessages" />
+
+    <!-- Tooltip -->
+    <div v-if="tooltipMessage" class="tooltip">{{ tooltipMessage }}</div>
   </div>
 </template>
 
@@ -23,8 +26,6 @@ import HeaderComponent from './HeaderComponent.vue';
 import LogComponent from './LogComponent.vue';
 import BoardComponent from './BoardComponent.vue';
 import ChatComponent from './ChatComponent.vue';
-
-
 
 export default {
   name: "BattleshipGame",
@@ -39,21 +40,20 @@ export default {
       wsService: null,
       player1: "xX_sampleUsername123_Xx",
       player2: "xX_sampleUsername345_Xx",
-      round: 1, //starts at round 1, goes up per turn done by player 2. could still be changed for if the first player to start is random.
-      turn: "P1", // could still be randomised when adding multiplayer
+      round: 1,
+      turn: "P1",
       score1: 0,
       score2: 0,
-      p1Moves: [], // list of all shots taken by player
-      p2Moves: [], // list of all shots taken by player
+      p1Moves: [],
+      p2Moves: [],
       p1Ships: [
-        //list of all ships to be used by the players.
         { name: "Carrier", size: 5, locations: [], placed: false },
         { name: "Battleship", size: 4, locations: [], placed: false },
         { name: "Cruiser", size: 3, locations: [], placed: false },
         { name: "Submarine", size: 3, locations: [], placed: false },
         { name: "Destroyer", size: 2, locations: [], placed: false }
       ],
-      p1Hits: [], //list of all hits taken by the player.
+      p1Hits: [],
       p2Ships: [
         { name: "Carrier", size: 5, locations: [], placed: false },
         { name: "Battleship", size: 4, locations: [], placed: false },
@@ -62,104 +62,69 @@ export default {
         { name: "Destroyer", size: 2, locations: [], placed: false }
       ],
       p2Hits: [],
-      chatMessages: [
-        {
-          user: "xX_sampleUsername123_Xx",
-          text: "worst player EUW servers? Uninstall the game please?",
-          class: "text-red-400"
-        },
-        { user: "xX_sampleUsername456_Xx", text: "Mad cuz bad, gg losing dog tier player.", class: "text-blue-400" }
-      ],
+      chatMessages: [],
       p1Phase: 'setup',
       p2Phase: 'setup',
       player2Type: 'CPU',
       tooltipMessage: null,
+      preMoves: [], // Tijdelijke opslag voor pre-moves
     };
   },
-  mounted() {
-  },
   methods: {
-    //method which is activated in the event that all ships have been placed from the player's side.
-    //needs to be fleshed out more still in case player is a real player.
     handleAllShipsPlaced(player) {
-
       if (player === 'p1') {
-        this.p1Phase = 'gameplay'; // change phase to gameplay for P1
-        // if P2 is CPU, randomize their ship placement
-
+        this.p1Phase = 'gameplay';
         if (this.player2Type === 'CPU') {
-          // searches the game's references for the p2 board property (see 2nd board html element)
-          // if player2 is a cpu, they don't get to decide their own board. thus it must be placed randomly.
           this.$refs.p2Board.randomizeShipPlacement();
-
         }
-        this.$webSocketService.sendMessage("/app/ships-placed", this.p1Ships);
-
       } else if (player === 'p2') {
-        //needs to be fleshed out still, for now
         this.p2Phase = 'gameplay';
-        this.$webSocketService.sendMessage("/app/ships-placed", this.p2Ships)
       }
 
-      // check if both players are ready to start (boards are set up).
       if (this.p1Phase === 'gameplay' && this.p2Phase === 'gameplay') {
         this.startGame();
       }
     },
 
-    //starts the game
     startGame() {
-      // reset scores / moves (game starts again).
       this.score1 = 0;
       this.score2 = 0;
       this.p1Moves = [];
       this.p2Moves = [];
     },
 
-    // manages turn logic. Activates after the current turn is still for player1, but the player's made their move.
     switchTurn() {
       if (this.turn === "P1") {
         this.turn = "P2";
-        // start cpu action if it's the CPU's turn
+        this.round++;
+        this.executePreMoves('p2');
         if (this.player2Type === 'CPU') {
-          // use setTimeout to create a delay before the CPU takes its turn, otherwise its your turn instantly after you took your turn again.
           setTimeout(() => {
-            this.cpuTakeTurn(); // call the CPU action after a delay
-          }, 2000); // delay 2 seconds
+            this.cpuTakeTurn();
+          }, 2000);
         }
       } else {
-        // switch back to player1, rounds continue.
         this.turn = "P1";
-        this.round++
+        this.executePreMoves('p1');
       }
     },
 
-    //selects a random location (a random id in the board) to shoot at.
-    cpuTakeTurn() {
-      //create variable to use for the randomly shot at id.
-      let cellId = Math.floor(Math.random() * 100) //gets random cellid till 100 (size of board)
-
-      // while the previously chosen cellId is either already included in the CPU's moves list, or the target (player 1)'s hits list
-      // finds a new cellId randomly till 100.
-      //this loops till a valid cellId is found (neither already made move before,
-      while (this.p2Moves.includes(cellId) || this.p1Hits.some(hit => hit.cellId === cellId)) {
-        cellId = Math.floor(Math.random() * 100);
-      }
-      // attempt the shot
-      console.log("CPU choosing cell: " + cellId);
-      this.takeShot('p2', cellId);
-    },
-
-    //shoots at the target @player 's board on the @cellId location
     takeShot(player, cellId) {
-      // Validatie: Controleer of de cel al geraakt is
+      if (this.turn !== (player === 'p1' ? 'P1' : 'P2')) {
+        this.preMoves.push({ player, cellId });
+        this.showTooltip("Pre-move saved! Will execute on your turn.");
+        return;
+      }
+      this.executeMove(player, cellId);
+    },
+
+    executeMove(player, cellId) {
       const hits = player === 'p1' ? this.p2Hits : this.p1Hits;
       if (hits.some(hit => hit.cellId === cellId)) {
         this.showTooltip("You already shot here!");
         return;
       }
 
-      // Voer het schot uit
       if (player === 'p1') {
         this.p1Moves.push(cellId);
         const hitShip = this.p2Ships.find(ship => ship.locations.includes(cellId));
@@ -180,51 +145,30 @@ export default {
         }
       }
 
-      // Wissel van beurt
       this.switchTurn();
     },
 
-    checkAllShipsSunk(player) {
-      const ships = player === 'p1' ? this.p1Ships : this.p2Ships;
-      const hits = player === 'p1' ? this.p1Hits : this.p2Hits;
+    executePreMoves(player) {
+      const moves = this.preMoves.filter(move => move.player === player);
+      moves.forEach(move => {
+        this.executeMove(move.player, move.cellId);
+      });
+      this.preMoves = this.preMoves.filter(move => move.player !== player);
+    },
 
-      return ships.every(ship =>
-        ship.locations.every(location =>
-          hits.some(hit => hit.cellId === location && hit.hit)
-        )
-      );
-    },
-    //ends the game and announces a winner, resets the game after clicking ok on the alert.
-    endGame(winner) {
-      alert(winner + " wins!");
-      this.resetGame();
-    },
-    //resets the game by resetting all relevant variables from before the game starts.
-    resetGame() {
-      this.p1Ships.forEach(ship => {
-        ship.locations = [];
-        ship.placed = false;
-      });
-      this.p2Ships.forEach(ship => {
-        ship.locations = [];
-        ship.placed = false;
-      });
-      this.p1Hits = [];
-      this.p2Hits = [];
-      this.p1Moves = [];
-      this.p2Moves = [];
-      this.p1Phase = 'setup';
-      this.p2Phase = 'setup';
-      this.score1 = 0;
-      this.score2 = 0;
-      this.turn = "P1";
+    cpuTakeTurn() {
+      let cellId = Math.floor(Math.random() * 100);
+      while (this.p2Moves.includes(cellId) || this.p1Hits.some(hit => hit.cellId === cellId)) {
+        cellId = Math.floor(Math.random() * 100);
+      }
+      this.takeShot('p2', cellId);
     },
 
     showTooltip(message) {
       this.tooltipMessage = message;
       setTimeout(() => {
         this.tooltipMessage = null;
-      }, 3000); // Verberg de tooltip na 3 seconden
+      }, 3000);
     },
   }
 };
