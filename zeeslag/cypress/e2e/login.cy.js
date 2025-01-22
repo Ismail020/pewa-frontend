5fe512d6a0780ca4c781d657cb35d01ce04997b2
Elena Lazareva <elena.lazareva@hva.nl>
2025-01-22 23:10:38 +0100

    Cypress test 1

--- zeeslag/cypress/e2e/login.cy.js
+++ zeeslag/cypress/e2e/login.cy.js
@@ -1,18 +1,26 @@
-describe('Login Function Test', () => {
+describe('Login Component Test', () => {
     it('checks if a button element has text', () => {
         cy.visit('localhost:5173/login')
-        cy.contains('button', 'LOGIN').should('exist')
+        cy.contains('button', 'Log in').should('exist')
     })
-    it('checks if the login redirects to /play on ok response from server and stays on same page otherwise', () => {
+    it('checks if the login redirects to "/selectgamemode" on ok response from server and stays on same page otherwise', () => {
         cy.visit('localhost:5173/login')
-        cy.get('input[type=text]').type('jd@gmail.com')
+        cy.get('input[type=email]').type('jd@gmail.com')
         cy.get('input[type=password]').type('password')
         cy.get('button[type="submit"]').click()
         cy.intercept('http://localhost:5173/login', () => {
             if (!response.ok) {
-                cy.url().contains('play')
+                cy.url().contains('selectgamemode')
             } else cy.url().contains('login')
-            cy.get('#hidden').invoke('show')
         })
     })
+    it ("checks for error message", () => {
+        cy.intercept('POST', '/api/login', {
+            statusCode: 200,
+            body: { token: 'fake-jwt-token' },
+        }).as('loginRequest');
+
+                cy.get(".message")
+                    .should("be.visible")
+        })
 })
