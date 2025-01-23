33a1d4a608e4f07bd12b34a79fda973ee0347884
Ismail Çetin <ismail.cetin@hva.nl>
2025-01-23 12:01:08 +0100

    Update dependencies and configure Node engine in package files

--- zeeslag/vite.config.js
+++ zeeslag/vite.config.js
@@ -1,22 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   server: {
-    port: 5173,
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   optimizeDeps: {
-    exclude: ['sockjs-client']
+    exclude: ["sockjs-client"],
   },
-})
+});
