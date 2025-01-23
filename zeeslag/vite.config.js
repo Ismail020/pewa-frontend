53c30ce08ef17122f3c6e2fcb960d1e2a1752024
Ismail Çetin <ismail.cetin@hva.nl>
2025-01-23 12:07:24 +0100

    Add preview configuration for Vite with custom host and port

--- zeeslag/vite.config.js
+++ zeeslag/vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
     host: "0.0.0.0",
     port: process.env.PORT || 5173,
   },
+  preview: {
+    host: "0.0.0.0",
+    port: 8080,
+    allowedHosts: ["pewa-frontend.onrender.com"],
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
